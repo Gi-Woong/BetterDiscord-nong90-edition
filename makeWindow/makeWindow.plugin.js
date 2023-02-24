672f4d29b5b9c61e487dfd236c45531235c78a53
pillose <pillose@pillose.kro.kr>
2023-02-24 20:48:52 +0900

    plugin update

--- makeWindow/makeWindow.plugin.js
+++ makeWindow/makeWindow.plugin.js
@@ -5,36 +5,83 @@
  * @version 0.0.1
  */
 
-const windowOptions = {
+const winOptions = {
     height : 40,
     width : 200,
     resizable : false,
 
     alwaysOnTop : true,
     minimizable : false,
-
+    skipTaskbar: true,
     frame : false,
 
-    opacity : 0.8
+    opacity : 0.8,
+
+    webPreferences : {
+        nodeIntegration: true
+    }
+};
+
+const html_path = process.env.APPDATA.replaceAll("\\", '/') + "/BetterDiscord/plugins/test.html";
+const html_url = "file:///" + html_path;
+
+const UI = BdApi.UI;
+
+class Window {
+    constructor() {
+        this.winId = null;
+    }
+
+    async open(url, options) {
+        this.winId = await UI.openWindow(url, options);
+    }
+
+    async exist() {
+        return await UI.existWindow(this.winId);
+    }
+
+    async close() {
+        return await UI.closeWindow(this.winId);
+    }
+
+    async run(script) {
+        return await UI.runWindow(this.winId, script);
+    }
 }
 
-const options = {
-    windowOptions : windowOptions
-}
-
-const path = process.env.APPDATA.replaceAll("\\", '/') + "/BetterDiscord/plugins/test.html"
-const url = "file:///" + path
-
-let win
+const windows = [];
 
 module.exports = class ExamplePlugin {
     constructor() {
     }
 
     start() {
-        win = BdApi.UI.openWindow(url, options)
+        windows.push(new Window());
+        windows[windows.length - 1].open(html_url, winOptions);
     } 
 
-    stop() {
+    async stop() {
+        for (let w of windows) {
+            if (await w.exist()) w.close();
+        }
+    }
+
+    observer() {
+        if (document.getElementById('guild-context') || document.getElementById('user-context')) {
+            //this.isOk = false;
+            var action_list = (document.getElementById('guild-context') !== null) ?
+                document.getElementById('guild-context').children[0] : document.getElementById('user-context').children[0];
+
+            if (action_list.childElementCount == 12) {
+                var sep = action_list.children[1].cloneNode(true);
+                var box = action_list.children[2].cloneNode(true);
+
+                box.children[0].id = "plugin-test";
+                box.children[0].children[0].innerText = "plugin test";
+
+                action_list.append(sep);
+                action_list.append(box);
+            }
+        }
     }
 }
